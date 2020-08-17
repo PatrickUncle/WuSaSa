253f103ef22eb8e578b7c0cd1e07f0cd707f59d8
As10906 <2403818207@qq.com>
2020-08-17 15:49:06 +0800

    完成了基本包的创建与数据库连接查询

--- src/main/java/com/wusasa/WusasaApplication.java
+++ src/main/java/com/wusasa/WusasaApplication.java
@@ -2,11 +2,13 @@ package com.wusasa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.wusasa.mapper")
 public class WusasaApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(WusasaApplication.class, args);
     }
-
 }
