97821a9b27dbd095be3e3bedfa6e9f97c7a2e3e2
unknown <2403818207@qq.com>
2020-08-17 14:33:15 +0800

    first commit

---
+++ src/main/java/com/wusasa/WusasaApplication.java
@@ -0,0 +1,12 @@
+package com.wusasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WusasaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WusasaApplication.class, args);
+    }
+
+}
